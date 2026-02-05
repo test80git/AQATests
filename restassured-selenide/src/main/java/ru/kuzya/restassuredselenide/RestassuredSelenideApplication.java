5a90e201e447f8d04af59d5cef28996f83b5a410
test80git <test80git@yandex.ru>
2026-02-05 22:16:56 +0300

    dev/restassured-selenide: init commit

---
+++ restassured-selenide/src/main/java/ru/kuzya/restassuredselenide/RestassuredSelenideApplication.java
@@ -0,0 +1,13 @@
+package ru.kuzya.restassuredselenide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestassuredSelenideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestassuredSelenideApplication.class, args);
+    }
+
+}
---
+++ restassured-selenide/src/test/java/ru/kuzya/restassuredselenide/RestassuredSelenideApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuzya.restassuredselenide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestassuredSelenideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
