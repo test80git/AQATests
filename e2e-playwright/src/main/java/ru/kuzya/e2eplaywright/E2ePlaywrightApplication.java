2b890e704f6d39f93d10b09471158858e0591306
test80git <test80git@yandex.ru>
2026-02-05 13:44:17 +0300

    dev/playwright: init commit

---
+++ e2e-playwright/src/main/java/ru/kuzya/e2eplaywright/E2ePlaywrightApplication.java
@@ -0,0 +1,13 @@
+package ru.kuzya.e2eplaywright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2ePlaywrightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E2ePlaywrightApplication.class, args);
+    }
+
+}
---
+++ e2e-playwright/src/test/java/ru/kuzya/e2eplaywright/E2ePlaywrightApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuzya.e2eplaywright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2ePlaywrightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
